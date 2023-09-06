603f8f66e5e52410c7119e8aba5308b94d3e3bb3
Sasin <Sasingudipati@hotmail.com>
2023-09-05 18:44:32 -0700

    example

--- MP1/example.py
+++ MP1/example.py
@@ -13,9 +13,9 @@ def tokens_lowercase(doc):
     #leave the rest of the code as is
     tok.set_content(doc.content())
     tokens, counts = [], []
-    for token, count in trigrams.items():
-        counts.append(count)
-        tokens.append(token)
+    # for token, count in trigrams.items():
+    #     counts.append(count)
+    #     tokens.append(token)
     return tokens
     
 if __name__ == '__main__':
